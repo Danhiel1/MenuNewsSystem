429aec67fa6d83b0924c9f9cf6c7af6ad024f566
Dani <99038783+Danhiel1@users.noreply.github.com>
2026-04-29 12:33:42 +0700

    fix(core): add missing IMenuReadRepository interface and implement in repository

--- Core.Application/Interfaces/IMenuReadRepository.cs
+++ Core.Application/Interfaces/IMenuReadRepository.cs
@@ -2,12 +2,6 @@ using Core.Application.ReadModels;
 
 namespace Core.Application.Interfaces
 {
-    /// <summary>
-    /// TẠI SAO IMenuReadRepository riêng?
-    /// - Giống IMenuRepository vs IGenericRepository
-    /// - IGenericReadRepository: CRUD chung cho MongoDB
-    /// - IMenuReadRepository: thêm methods đặc thù cho Menu reads (GetAll, GetById)
-    /// </summary>
     public interface IMenuReadRepository
     {
         Task<IEnumerable<MenuReadModel>> GetAllAsync();
